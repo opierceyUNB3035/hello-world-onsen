9a11becdae9a9616985750a1c2a9459067289c15
Owen Piercey <opiercey@unb.ca>
2023-11-07 10:04:04 -0400

    Init push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+import { Page, Toolbar, Content } from 'react-onsenui';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page renderToolbar={() =>
+      <Toolbar>
+        <div className="center">Hello World App</div>
+      </Toolbar>
+    }>
+      <div style={{ textAlign: 'center', marginTop: '45px' }}> {/* Adjust the marginTop as needed */}
+        Hello World!
+      </div>
+    </Page>
   );
 }
 
 export default App;
+
